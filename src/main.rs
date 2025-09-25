787326cf491ae82aab9f3a7370e4c9e60407f620
Joel Williams <joel@reboundstudios.com>
2025-09-25 23:07:58 +0900

    max width and height. expanded charset

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::env;
 
-use image::{self, GrayImage, Pixel};
+use image::{self, GrayImage};
 
 fn img_to_ascii(file_path: &str) -> Result<String, image::ImageError> {
 
     const ASCII_VALUES: [char; 10] = ['@','#','S','%','?','*','+',';','.', ' '];
+    let dimensions: (u32, u32) = (100, 100);
     
     let img = image::ImageReader::open(file_path)?
         .decode().expect("Couldn't open file: {:file_path?}");
@@ -13,8 +14,8 @@ fn img_to_ascii(file_path: &str) -> Result<String, image::ImageError> {
     let (w, h) = grayscale_img.dimensions();
     let mut ascii_img = String::with_capacity((w * h + h) as usize);
 
-    for y in (0..h).step_by(8) {
-        for x in (0..w).step_by(4) {
+    for y in (0..h).step_by((h/(dimensions.1 / 2)) as usize) {
+        for x in (0..w).step_by((w/dimensions.0) as usize) {
             let lum = grayscale_img.get_pixel(x, y).0[0] as usize;
             let index = lum * (ASCII_VALUES.len() - 1) / 255;
             ascii_img.push(ASCII_VALUES[index]);
@@ -34,5 +35,4 @@ fn main() {
     let img = img_to_ascii(file_path).unwrap();
     
     println!("{}", img)
-
 }
